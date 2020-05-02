57e894194fc221db3649b78ba26f231a95d1605e
yasemin-albanna <albannay@mcmaster.ca>
2020-05-02 17:08:00 -0400

    Created App tabs

---
+++ lib/MentalTab.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class Mental extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
---
+++ lib/PhysicalTab.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class Physical extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
